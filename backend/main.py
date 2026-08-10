e2edef09fe8296a79f22afca07d79228502304bf
Firman Wijaya Kusuma Wijaya Kusuma <firmanwijayakusuma11@gmail.com>
2026-08-11 05:28:10 +0700

    update project files

--- backend/main.py
+++ backend/main.py
@@ -39,7 +39,7 @@ async def evaluate_candidate(data: EvaluationRequest):
     
     try:
         response = client.models.generate_content(
-            model = genai.GenerativeModel('gemini-pro'), 
+            model = "genai-3.5-turbo", 
             contents=prompt,
             config={
                 'response_mime_type': 'application/json' 
